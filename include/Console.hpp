8fc8b4bdc017c65df01eb7475aa8b766c179d759
FujiwaraChoki <sami@samihindi.com>
2024-05-08 18:56:31 +0200

    refactor: Remove termcolor as lib, add only header

--- include/Console.hpp
+++ include/Console.hpp
@@ -1,3 +1,31 @@
+#include "termcolor.hpp"
+
 class Console
 {
+public:
+    /* Constructor */
+    Console();
+
+    /* Destructor */
+    ~Console();
+
+    /* Starts the console */
+    void start();
+
+    /* Stops the console */
+    void stop();
+
+    /* Take user input */
+    template <typename T>
+    T input(std::string message)
+    {
+        T value;
+        std::cout << message;
+        std::cin >> value;
+        return value;
+    }
+
+private:
+    bool isRunning;
+    std::string currentPath;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,47 @@
+#include <stdint.h>
+
+#include "termcolor.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
 
+#include "console.hpp"
+
+std::string getRootDir()
+{
+	/* Finds the path to the assets folder dynamically. */
+	std::string ROOT_PATH = __FILE__;
+
+	// Find the last occurrence of the path separator
+	size_t found = ROOT_PATH.find_last_of("/\\");
+
+	// Keep looping until \\SkibiDB is found
+	while (ROOT_PATH.substr(found) != "/SkibiDB")
+	{
+		// Remove the last directory from the path
+		ROOT_PATH = ROOT_PATH.substr(0, found);
+
+		// Find the last occurrence of the path separator
+		found = ROOT_PATH.find_last_of("/\\");
+	}
+
+	// Return the root path
+	return ROOT_PATH;
+}
+
 void printAscii()
 {
-	/*
-	Prints the Skibidi Toilet ASCII Art.
-	*/
+	/* Prints the Logo ASCII Art. */
+	std::string asciiArtPath = getRootDir() + "/assets/art.txt";
+
 	// Read from assets/art.txt
-	std::ifstream file("assets/art.txt");
+	std::ifstream file(asciiArtPath);
 
 	// Print the ASCII Art
 	std::string line;
 	while (std::getline(file, line))
 	{
-		std::cout << line << std::endl;
+		std::cout << termcolor::on_blue << line << termcolor::reset << std::endl;
 	}
 
 	// Close the file
@@ -23,7 +50,13 @@ void printAscii()
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	printAscii();
+
+	// Create a new console
+	Console console;
+
+	// Run the console
+	console.start();
 
 	return 0;
 }
